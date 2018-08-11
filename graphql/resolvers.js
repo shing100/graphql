e5c89e0e69440ad0564e77ef3b124885c0993d52
shing100 <shing100@naver.com>
2018-08-11 13:57:47 +0900

    limit , rating 추가

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -3,8 +3,8 @@ import { getMovies } from "./db";
 // 리졸버 지정을 해줘야함
 const resolvers = {
     Query: {
-        movies: () => getMovies()
+        movies: (_, {rating, limit}) => getMovies(limit, rating)
     }
 }; 
- 
+  
 export default resolvers;
