00c49410e90d145c576094147689b208b2bbd899
shing100 <shing100@naver.com>
2018-08-11 14:30:26 +0900

    update

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,4 +1,4 @@
-import { getMovies, getMovie } from "./db";
+import { getMovies, getMovie, getSuggestions } from "./db";
 
 // 리졸버 지정을 해줘야함
 const resolvers = {
@@ -7,6 +7,6 @@ const resolvers = {
         movie: (_, { id }) => getMovie(id),
         suggestions: (_, { id }) => getSuggestions(id)
     }
-}; 
+};
   
 export default resolvers;
